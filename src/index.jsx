5cd5b5d99570312297124d52f0689327b07a623a
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2021-06-28 19:37:14 +0500

    refactor: mock api requests

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom';
 
+import mswWorker from './apiMock/browser';
 import App from './App';
 import { StoreProvider } from './store';
+
+mswWorker.start({
+  onUnhandledRequest: 'bypass',
+  quiet: process.env.NODE_ENV !== 'development',
+});
 
 ReactDOM.render(
   <StoreProvider>
