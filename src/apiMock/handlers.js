c410503925e04b83e7e76a1ccd44e09c9f04c59f
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2021-06-29 11:21:20 +0500

    refactor: add filtering for api

--- src/apiMock/handlers.js
+++ src/apiMock/handlers.js
@@ -1,3 +1,4 @@
+import Fuse from 'fuse.js';
 import { rest } from 'msw';
 
 import data from './data.json';
@@ -5,15 +6,45 @@ import data from './data.json';
 const url = 'https://jobs.github.com';
 const itemsInPage = 9;
 
+const filter = (description, location, fullTime) => {
+  const filteredByContract = fullTime
+    ? data.filter(({ contract }) => contract === 'Full Time')
+    : data;
+
+  const filteredByContractAndLocation = location
+    ? filteredByContract.filter(
+        (job) => job.location.toLowerCase() === location.toLowerCase(),
+      )
+    : filteredByContract;
+
+  if (!description) return filteredByContractAndLocation;
+
+  const fuse = new Fuse(filteredByContractAndLocation, {
+    keys: [
+      'company',
+      'description',
+      'position',
+      'requirements.content',
+      'requirements.items',
+      'role.content',
+      'role.items',
+    ],
+  });
+
+  return fuse.search(description).map(({ item }) => item);
+};
+
 export default [
   rest.get(`${url}/positions.json`, (req, res, ctx) => {
     const params = req.url.searchParams;
 
     const pageParam = Number(params.get('page'));
     const page = Number.isNaN(pageParam) || pageParam < 1 ? 1 : pageParam;
+    const description = params.get('search');
+    const location = params.get('location');
+    const fullTime = params.get('full_time') === 'on';
 
-    // TODO
-    const filtered = data;
+    const filtered = filter(description, location, fullTime);
 
     return res(
       ctx.status(200),
