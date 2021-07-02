8ed2748f3e6c495aa8f40a86ead61a7bc8d9a65e
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2021-07-02 10:51:43 +0500

    refactor: convert promises to async await syntax

--- src/store/job/job.slice.js
+++ src/store/job/job.slice.js
@@ -13,13 +13,12 @@ const initialState = {
   job: null,
 };
 
-export const fetchJob = createAsyncThunk('job/fetch', (id) =>
-  fetch(`https://jobs.github.com/positions/${id}.json`).then((response) => {
-    if (response.status === 404)
-      throw new Error(`The jobs with id ${id} is not found`);
-    return response.json();
-  }),
-);
+export const fetchJob = createAsyncThunk('job/fetch', async (id) => {
+  const response = await fetch(`https://jobs.github.com/positions/${id}.json`);
+  if (response.status === 404)
+    throw new Error(`The job with id ${id} is not found`);
+  return response.json();
+});
 
 const jobSlice = createSlice({
   name: 'job',
