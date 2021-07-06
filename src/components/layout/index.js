c78145e1debfc9e9eae284822dcb97e38e885605
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2021-07-06 19:05:56 +0500

    refactor: search form

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,5 +2,6 @@ import AppContainer from './AppContainer/AppContainer';
 import Container from './Container/Container';
 import Header from './Header/Header';
 import ScrollToTop from './ScrollToTop/ScrollToTop';
+import Separator from './Separator/Separator';
 
-export { AppContainer, Container, Header, ScrollToTop };
+export { AppContainer, Container, Header, ScrollToTop, Separator };
