3496e1ec518a4fa21cf3e2639a97d24eb85beb63
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2021-07-08 23:04:50 +0500

    test: add home page first load tests

--- src/views/Home/HomePage/HomePage.test.jsx
+++ src/views/Home/HomePage/HomePage.test.jsx
@@ -8,36 +8,56 @@ import { setupServer } from 'msw/node';
 import { Router } from 'react-router-dom';
 
 import data from '@/__mocks__/jobsApi/data.json';
+import { ITEMS_IN_PAGE } from '@/__mocks__/jobsApi/handlers';
 
 import HomePage from './HomePage';
 
 matchMediaPolyfill(window);
-const api = 'https://jobs.github.com';
+const API = 'https://jobs.github.com';
 
-const server = setupServer(
-  rest.get(`${api}/positions.json`, (req, res, ctx) => {
-    if (req.url.searchParams.get('location') === 'russ')
-      return res(ctx.json(data.filter((job) => job.location.includes('Russ'))));
-    return res(ctx.json(data.slice(0, 9)));
-  }),
-);
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
-
-test('HomePage loads and displays search form and list of jobs', async () => {
-  const history = createMemoryHistory();
-  // history.push('/?location=russ');
-  await renderPage(
-    <Router history={history}>
-      <HomePage />
-    </Router>,
+describe('Home page', () => {
+  const server = setupServer(
+    rest.get(`${API}/positions.json`, (req, res, ctx) =>
+      res(ctx.json(data.slice(0, ITEMS_IN_PAGE))),
+    ),
   );
 
-  expect(screen.getByRole('form')).toBeInTheDocument();
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
 
-  const jobList = await screen.findByRole('list');
-  expect(jobList).toBeInTheDocument();
-  expect(within(jobList).getAllByRole('listitem')).toHaveLength(9);
+  it('loads and displays search form and list of jobs', async () => {
+    renderPage(
+      <Router history={createMemoryHistory()}>
+        <HomePage />
+      </Router>,
+    );
+
+    expect(screen.getByRole('form')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /search/i })).toBeDisabled();
+
+    const jobList = await screen.findByRole('list');
+    expect(jobList).toBeInTheDocument();
+    expect(within(jobList).getAllByRole('listitem')).toHaveLength(
+      ITEMS_IN_PAGE,
+    );
+  });
+
+  it('shows feedback on fetch error', async () => {
+    server.use(
+      rest.get(`${API}/positions.json`, (req, res, ctx) =>
+        res.once(ctx.status(500)),
+      ),
+    );
+
+    renderPage(
+      <Router history={createMemoryHistory()}>
+        <HomePage />
+      </Router>,
+    );
+
+    expect(await screen.findByRole('alert')).toHaveTextContent(
+      'Error while getting jobs, please try again',
+    );
+  });
 });
